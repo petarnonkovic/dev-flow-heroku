2d64524588f105ed45c363c205af0498d9c6b136
petroniuss <tekumseh777@gmail.com>
2019-06-18 13:27:11 +0200

    port added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const http = require('http')
 
 const host = '127.0.0.1'
-const PORT = process.env.PORT || 3300
+const PORT = 3300
 
 const server = http.createServer((req, res) => {
     res.statusCode = 200
